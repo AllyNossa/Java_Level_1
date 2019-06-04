a568cbbf77be365db7fbb463a04318eecd6ed2aa
Alla Nosova <aknosova@avito.ru>
2019-06-04 13:30:39 +0300

    Домашка 5

--- src/lesson5_2/ArrayMath.java
+++ src/lesson5_2/ArrayMath.java
@@ -1,10 +1,10 @@
 package lesson5_2;
 
-public class ArrayWithoutThread {
+public class ArrayMath {
     static final int SIZE = 10000000;
     static final int HALF = SIZE / 2;
 
-    static void arraySet(float[] arr) {
+    static void arraySetWithouThread(float[] arr) {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = 1;
         }
@@ -18,26 +18,28 @@ public class ArrayWithoutThread {
         System.out.println(System.currentTimeMillis()- a);
     }
 
-    static void arraySetThread(float[] arr, float[] arr1, float[] arr2, int size, Thread thread, Thread thread2) {
+    static void arraySetThread(float[] arr, float[] arr1, float[] arr2, int size) {
         long a = System.currentTimeMillis();
 
         System.arraycopy(arr, 0, arr1, 0, size);
         System.arraycopy(arr, size, arr2, 0, size);
 
-        thread.start();
+        Thread thread1 = new Thread();
+        Thread thread2 = new Thread();
+
+        thread1.start();
+        thread2.start();
 
         for (int i = 0; i < arr1.length; i++) {
             arr1[i] = 1;
         }
 
-        for (int i = 0; i < arr2.length; i++) {
-            arr2[i] = 1;
-        }
-
-        thread2.start();
-
         for (int i = 0; i < arr1.length; i++) {
             arr1[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = 1;
         }
 
         for (int i = 0; i < arr1.length; i++) {
@@ -55,21 +57,13 @@ public class ArrayWithoutThread {
         float[] arr1 = new float[HALF];
         float[] arr2 = new float[HALF];
 
-        arraySet(arr);
+        arraySetWithouThread(arr);
 
-        MyThreads myThread = new MyThreads();
-        MyThreads myThread2 = new MyThreads();
-
-        Thread thread = new Thread(myThread);
-        Thread thread2 = new Thread(myThread2);
-
-        arraySetThread(arr, arr1, arr2, HALF, thread, thread2);
+        arraySetThread(arr, arr1, arr2, HALF);
     }
 }
 
-class MyThreads extends Thread {
 
-}
 
 
 
