bbbe1f14bf84ee1ac4adbacfa7ec8b8d9d202125
Alla Nosova <aknosova@avito.ru>
2019-06-07 18:28:42 +0300

    Домашка 5

--- src/lesson5_2/ArrayMath.java
+++ src/lesson5_2/ArrayMath.java
@@ -40,7 +40,7 @@ public class ArrayMath {
             }
 
             for (int i = 0; i < arr.length; i++) {
-                arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                arr[i] = (float) (arr[i] * Math.sin(0.2f + (i + HALF)/ 5) * Math.cos(0.2f + (i + HALF)/ 5) * Math.cos(0.4f + (i + HALF) / 2)); 
             }
         }
     }
